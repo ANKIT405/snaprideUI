82bdcb8e170165516db39bd4c58eb7674d0cbc06
ANKIT KUMAR <136682826+ANKIT405@users.noreply.github.com>
2024-07-18 23:30:33 +0530

    Update Payment.js

--- src/Components/Payment.js
+++ src/Components/Payment.js
@@ -1,9 +1,29 @@
-import React from 'react'
-import '../Components-css/Payment.css'
 const Payment = () => {
+
+        let Details={
+          method:"POST",
+          headers:{"Content-Type":"application/json"},
+          body:JSON.stringify({
+            Pickup:sessionStorage.getItem("inp1"),
+            Destination:sessionStorage.getItem("inp2"),
+            Time:sessionStorage.getItem("Pick me"),
+            Passenger:sessionStorage.getItem("Passenger")
+          })
+        }
+        fetch("https://snapride-server.onrender.com/Email",Details).then((res)=>{
+             
+            alert(res)
+        }).catch((err)=>{
+          alert("Please try again after Sometimes")
+        })
+
+
   return (
-    <div>
-     <h1>Payment</h1> 
+    <div className="payment-container">
+      <div className="thank-you-message">
+        <h1>Thank You!</h1>
+        <p>We appreciate your ride with SnapRide.</p>
+      </div>
     </div>
   )
 }
